64f79029d81252c14cac72c391a085624996c73d
gallg <giuseppe.gallitto01@gmail.com>
2024-05-01 16:34:51 +0200

    corrected for submission

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'External validation of machine learning models with adaptive
 # Setting up
 setup(
     name="adaptivesplit",
-    version=VERSION,
+    version="0.0.1",
     author="PNI Lab (Predictive NeuroImaging Laboratory)",
     author_email="<giuseppe.gallitto@uk-essen.de>",
     description=DESCRIPTION,
@@ -15,7 +15,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=["joblib==1.2.0", "matplotlib==3.6.0", "numpy==1.24.2", "pandas==1.5.1", "pygam==0.9.0",
-                      "regressors==0.0.3", "scikit_learn==1.2.1", "scipy==1.11.3", "tqdm==4.64.1"],
+                      "regressors==0.0.3", "scikit_learn==1.2.1", "scipy==1.11.3", "tqdm==4.64.1", "setuptools==57.5.0"],
     keywords=['python', 'machine learning', 'neuroimaging', 'data splitting'],
     classifiers=[]
 )
